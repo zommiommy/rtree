61b3ce220deefc2fba7219d19ee2cc68b885ff07
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2023-05-02 20:43:51 +0200

    added masked test

--- tests/test_radix_tree.rs
+++ tests/test_radix_tree.rs
@@ -11,3 +11,17 @@ pub fn test_radix_tree() {
 
     assert_eq!(element, tree.get(hash).unwrap()[0]);
 }
+
+
+#[test]
+pub fn test_radix_tree_masked() {
+    let mut tree: RadixTree<u64> = RadixTree::new();
+
+    tree.insert(1, 0b1);
+    tree.insert(2, 0b0);
+
+    assert_eq!(vec![vec![1], vec![2]], tree.get_masked(0b1, 0b1));
+    assert_eq!(vec![vec![1], vec![2]], tree.get_masked(0b0, 0b1));
+    assert_eq!(vec![vec![1]], tree.get_masked(0b1, 0b0));
+    assert_eq!(vec![vec![2]], tree.get_masked(0b0, 0b0));
+}
